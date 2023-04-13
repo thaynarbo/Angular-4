cf5b4c84fac7293da89b86be731a023929b8f112
Thaynar Brandão <thaynar.lima@db1.com.br>
2023-04-12 23:21:13 -0300

    feat: add main pages and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpModule } from "@angular/http";
+import { RouterModule } from "@angular/router";
 
-
-import { AppComponent } from './app.component'
-
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { HeaderComponent } from "./header/header.component";
+import { ROUTES } from "./app.routes";
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
+import { RestaurantComponent } from "./restaurants/restaurant/restaurant.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
+    RestaurantsComponent,
+    RestaurantComponent,
   ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    RouterModule
-  ],
+  imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
