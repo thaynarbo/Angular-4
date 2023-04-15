e43786d3a2a5386f45b9d0fb85c7da55f364336a
Thaynar Brandão <thaynar.lima@db1.com.br>
2023-04-15 09:10:42 -0300

    feat: change mock by injecting http service

---
+++ src/app/restaurants/restaurants.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { Restaurant } from "./restaurant/restaurant.model";
+import { Http } from "@angular/http";
+import { MEAT_API } from "app/app.api";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+
+import { ErrorHandler } from "app/app.error-handler";
+
+@Injectable()
+export class RestaurantsService {
+  constructor(private http: Http) {}
+
+  restaurants(): Observable<Restaurant[]> {
+    return this.http
+      .get(`${MEAT_API}/restaurants`)
+      .map((response) => response.json())
+      .catch(ErrorHandler.handleError);
+  }
+  restaurantById(id: string): Observable<Restaurant> {
+    return this.http
+      .get(`${MEAT_API}/restaurants/${id}`)
+      .map((response) => response.json())
+      .catch(ErrorHandler.handleError);
+  }
+}
